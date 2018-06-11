dedb892f72ed0bf8f2d6cf6cad6b9c397210f173
Tim Rooney <timothyrooney93@gmail.com>
2018-06-11 12:39:50 -0500

    Changed default font size in emacs

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -151,7 +151,7 @@ values."
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
    dotspacemacs-default-font '("Fira Mono"
-                               :size 18
+                               :size 16
                                :weight normal
                                :width normal
                                :powerline-scale 1.1)
