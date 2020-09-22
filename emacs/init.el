2f61e541fed72f65aae0358ffce290c2ff5a981d
Tim Rooney <timothyrooney93@gmail.com>
2020-09-22 16:10:18 -0500

    configured org-mode agenda

--- emacs/init.el
+++ emacs/init.el
@@ -14,18 +14,20 @@
  '(ansi-color-names-vector
    (vector "#ffffff" "#c82829" "#718c00" "#eab700" "#4271ae" "#8959a8" "#3e999f" "#4d4d4c"))
  '(beacon-color "#c82829")
- '(custom-enabled-themes '(sanityinc-tomorrow-bright))
+ '(custom-enabled-themes '(gruvbox-light-soft))
  '(custom-safe-themes
    '("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "4cf9ed30ea575fb0ca3cff6ef34b1b87192965245776afa9e9e20c17d115f3fb" "e1d09f1b2afc2fed6feb1d672be5ec6ae61f84e058cb757689edb669be926896" "123a8dabd1a0eff6e0c48a03dc6fb2c5e03ebc7062ba531543dfbce587e86f2a" "939ea070fb0141cd035608b2baabc4bd50d8ecc86af8528df9d41f4d83664c6a" "aded61687237d1dff6325edb492bde536f40b048eab7246c61d5c6643c696b7f" default))
  '(fci-rule-color "#d6d6d6")
  '(flycheck-color-mode-line-face-to-color 'mode-line-buffer-id)
  '(frame-background-mode 'light)
  '(global-display-line-numbers-mode t)
+ '(helm-completion-style 'emacs)
  '(inhibit-startup-screen t)
  '(org-agenda-files
-   '("~/OneDrive - A.O. Smith Corporation/work/HVAC Course/Day2.org"))
+   '("~/Notes/TankModeling.org" "~/Notes/Field-Data.org" "~/Notes/UEF.org" "~/Notes/Tube-in-Tube.org" "~/Notes/HPWHSizer.org" "~/Notes/Gas.org" "~/Notes/GT-SUITE.org" "~/Notes/CBECC.org" "~/Notes/admin.org"))
+ '(org-latex-compiler "xelatex")
  '(package-selected-packages
-   '(evil-easymotion org-autolist csv-mode evil-magit color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized helm-projectile magit company-jedi gruvbox-theme neotree which-key autopair evil-surround evil-escape rainbow-delimiters company helm-slime slime evil-collection evil))
+   '(evil-org evil-easymotion org-autolist csv-mode evil-magit color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized helm-projectile magit company-jedi gruvbox-theme neotree which-key autopair evil-surround evil-escape rainbow-delimiters company helm-slime slime evil-collection evil))
  '(pdf-view-midnight-colors '("#fdf4c1" . "#282828"))
  '(vc-annotate-background nil)
  '(vc-annotate-color-map
@@ -54,7 +56,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "DejaVu Sans Mono" :foundry "outline" :slant normal :weight normal :height 113 :width normal)))))
+ '(default ((t (:family "DejaVu Sans Mono" :foundry "outline" :slant normal :weight normal :height 98 :width normal)))))
 
 ;; Hook configuration
 (add-hook 'after-init-hook 'global-company-mode)
@@ -72,7 +74,7 @@
 (evilem-default-keybindings "SPC")
 
 ;; Theme
-(load-theme 'sanityinc-tomorrow-day t)
+;; (load-theme 'sanityinc-tomorrow-day t)
 
 ;; General helpers
 (global-set-key [f8] 'neotree-toggle)
@@ -99,8 +101,17 @@
 
 ;; Org-mode configuration
 (add-hook 'org-mode-hook (lambda () (org-autolist-mode)))
+(add-hook 'org-mode-hook 'evil-org-mode)
+(add-hook 'org-mode-hook (lambda ()
+			   (setq truncate-lines nil)
+			   (setq word-wrap t)
+			   (flyspell-mode 1)))
+(setq org-image-actual-width '(300))
+(add-hook 'evil-org-mode-hook (lambda ()
+				(evil-org-set-key-theme)
+				(require 'evil-org-agenda)
+				(evil-org-agenda-set-keys)))
 (setq org-html-validation-link nil)
-(setq org-odt-preferred-output-format "docx")
 
 ;; Git configuration
 (global-set-key (kbd "C-x g") 'magit-status)
